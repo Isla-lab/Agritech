0a390f3e1a2001369a29caeeda4179fcf8de5c28
danm11694 <102983945+danm11694@users.noreply.github.com>
2026-05-07 18:47:12 +0200

    Simplify update process in start_scoperta_ide.sh
    Replaced git commands with a single git pull for simplicity.

--- start_scoperta_ide.sh
+++ start_scoperta_ide.sh
@@ -2,10 +2,6 @@
 
 notify-send -a ScopertaIDE -i $PWD/scoperta_ide/icon.png "Avvio di ScopertaIDE" "Verifica degli aggiornamenti in corso. Per favore, attendi..."
 
-git fetch
-git stash push -m "ScopertaIDE Updater Backup"
-git reset --hard origin/main
-git stash apply
-git stash clear
+git pull
 
 scoperta_ide/scoperta_ide
